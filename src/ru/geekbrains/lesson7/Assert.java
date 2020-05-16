0e6eb6a0c3933008360367634cb51ffee30a1451
Valery Bulekov <muzaccord@gmail.com>
2020-05-16 14:05:22 +0300

    Homework #6. Tests

---
+++ src/ru/geekbrains/lesson7/Assert.java
@@ -0,0 +1,24 @@
+package ru.geekbrains.lesson7;
+
+public class Assert {
+
+    private Assert(){
+
+    }
+
+    public static void assertBoolean(boolean expected, boolean condition){
+        if (expected==condition) {
+            System.out.println("Success!");
+        } else {
+            System.out.println("Fail!");
+        }
+    }
+
+    public static void assertEquals(Object expected, Object condition){
+        if (expected.equals(condition)) {
+            System.out.println("Success!");
+        } else {
+            System.out.println("Fail!");
+        }
+    }
+}
---
+++ src/ru/geekbrains/lesson7/ProgramForTesting.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.lesson7;
+
+public class ProgramForTesting {
+    public int add (int a, int b){
+        return a+b;
+    }
+
+    public boolean iaAMoreThenB(int a, int b){
+        return a>b;
+    }
+}
